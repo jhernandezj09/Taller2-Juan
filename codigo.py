f1c6630f9f5d59be6c11b2a2e559d05d6a57ded9
j.hernandezj09 <j.hernandezj@uniandes.edu.co>
2026-01-30 21:42:36 -0500

    se cambio el valor de la variable prueba y se imprimio el valor de la llave "hola" del diccionario nueva_variable

--- codigo.py
+++ codigo.py
@@ -1,4 +1,4 @@
-prueba = 3
+prueba = 7
 lista = [1,2,3,4,5]
 for i in lista:
     prueba += i
@@ -6,5 +6,6 @@ for i in lista:
 print(prueba)
 
 "se realizan cambios al codigo"
-
+suma = 0
 nueva_variable = {"hola": 1, "chao": 2}
+print(nueva_variable["hola"])
